7f735bc1cef2f5ffc4507377e24f534b4adcf9c1
decoder-25 <bidisha2125@gmail.com>
2024-05-14 21:16:34 +0530

    Commit changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import Register from './pages/Register';
 import Login from './pages/Login';
+
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Register/>
     </div>
   );
 }
