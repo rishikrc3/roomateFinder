18ad3366ff4bd726da5133c4e10c9a743cef0a94
Kabir Pramanik <pmkabir01@gmail.com>
2024-05-23 22:28:28 +0530

    no such significant commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 import "./home.css";
 import homeImage from "../images/home.jpg";
+import logo from "../images/logo.png" // Import the logo image
+
 const Home = () => {
-  return (
-    <section className="container">
-      <div className="left-half">
-        <div className="writeup1">
-          <h1>Hey! You are in the right place</h1>
-        </div>
-        <div className="writeup2">
-          <h3>
-            Talk with different people while exploring the app and enjoy
-            choosing your 
-            <span> Roomamte!</span>
-          </h3>
-        </div>
-        <div className="bottom">
-          <button className="button"><span> Getting Started</span></button>
-        </div>
-      </div>
-      <div className="right-half">{<img src={homeImage} alt="Login" />}</div>
-    </section>
-  );
+    return (
+        <section className="container">
+            {/* <div className="logo">
+            </div> */}
+            <div className="left-half">
+                <img src={logo} alt="Logo" /> {/* Render the logo image */}
+                <div className="writeup1">
+                    <h1>Hey! You are at the right place</h1>
+                </div>
+                <div className="writeup2">
+                    <h3>
+                        Talk with different people while exploring the app and enjoy
+                        choosing your
+                        <span> Roomamte!</span>
+                    </h3>
+                </div>
+                <div className="bottom">
+                    <button className="button"><span> Getting Started</span></button>
+                </div>
+            </div>
+            <div className="right-half">{<img src={homeImage} alt="Login" />}</div>
+        </section>
+    );
 };
 
 export default Home;
