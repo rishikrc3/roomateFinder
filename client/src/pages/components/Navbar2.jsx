520fafabb963cdf9454f70b93d1d0a1203edacb3
Kabir Pramanik <pmkabir01@gmail.com>
2024-05-27 00:10:16 +0530

    change

--- client/src/pages/components/Navbar2.jsx
+++ client/src/pages/components/Navbar2.jsx
@@ -29,7 +29,7 @@ function Navbar() {
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
             Nest
-            <i className='fas fa-home' />
+            {/* <i className='fas fa-home' /> */}
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
