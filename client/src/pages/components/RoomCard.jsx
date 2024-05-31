c377c8638147d74bfcb9f3d63dfc5c54ddb61f6a
Sudipta Patra <sudiptasumi2001@gmail.com>
2024-05-31 11:30:26 +0530

    changes

--- client/src/pages/components/RoomCard.jsx
+++ client/src/pages/components/RoomCard.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import './RoomCard.css'
 
-const RoomCard = ({ fullName, messName, location, rent, lookingFor }) => {
+const RoomCard = ({  messName, location, rent, lookingFor }) => {
   return (
     <div className="room-card">
-      <h2 className="room-card-title"><strong></strong> {fullName}</h2>
       <p className="room-card-detail"><strong>Mess Name:</strong> {messName}</p>
       <p className="room-card-detail"><strong>Location:</strong> {location}</p>
       <p className="room-card-detail"><strong>Rent:</strong> {rent}</p>
--- client/src/pages/components/RoomMateCard.jsx
+++ client/src/pages/components/RoomMateCard.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import './RoomCard.css'
 
-const RoommateCard = ({ fullName, location, rent, lookingFor }) => {
+const RoommateCard = ({ location, rent, lookingFor }) => {
   return (
     <div className="room-card">
-      <h2 className="room-card-title"><strong></strong> {fullName}</h2>
-      <p className="room-card-detail"><strong>Location:</strong> {location}</p>
-      <p className="room-card-detail"><strong>Rent:</strong> {rent}</p>
+      <p className="room-card-detail"><strong>Preferred Location:</strong> {location}</p>
+      <p className="room-card-detail"><strong>Preferred Rent:</strong> ₹{rent}</p>
       <p className="room-card-detail"><strong>Looking For:</strong> {lookingFor}</p>
       <p className="room-card-detail"><strong>Looking For:</strong> Room </p>
     </div>
